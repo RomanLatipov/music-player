ff943605837f9ca54d62811f2397b3a202216aee
RomanLatipov <61292132+RomanLatipov@users.noreply.github.com>
2025-03-12 22:45:04 -0400

    create layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css'
 
 function App() {
-  return (<></>)
+
+  return (
+    <>
+    <div id="sidebar"></div>
+    <div id="header"> </div>
+    <div id="main"></div>
+    <div id="bottom-bar"></div>
+    </>
+  )
 }
 
 export default App
